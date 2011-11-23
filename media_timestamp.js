11c03897a4c514877753318a330b62d00e33ff9b
Noah Adler <noah.adler@uky.edu>
2011-11-23 14:45:21 -0500

    Scrolling, with no interpolation, to position the most recent timestamp marker at the top of each pane

--- media_timestamp.js
+++ media_timestamp.js
@@ -21,7 +21,26 @@ jQuery(document).ready(function() {
 		setInterval(function() {
 				// TODO: implement scrolling to keep track of time
 				//console.log('play position: ' + player.media.currentTime + ' of ' + player.media.duration);
-		}, 500);
+				// TODO: div.scrollable text should be a text render style ideally - currently implemented directly in theme templates
+				var currentTime = player.media.currentTime;
+				jQuery('.scrolling-text').each(function(index, el) {
+					var timestamps = jQuery(el).find('a.timestamp');
+					var prevTs = null
+					var nextTs = null;
+					timestamps.each(function(ti,tel) {
+						if (parseInt(jQuery(tel).attr('timestamp')) <= currentTime)
+						{
+							prevTs = jQuery(tel);
+
+						}
+					});
+					if (prevTs !== null)
+					{
+						jQuery(el).animate({scrollTop: prevTs.position().top + jQuery(el).scrollTop() - jQuery(el).position().top }, 1000);
+					}
+
+				});
+		}, 1000);
 
 		// find timestamps and convert them to links
 		var timerail = jQuery('.mejs-time-rail');
@@ -42,7 +61,7 @@ jQuery(document).ready(function() {
 				'left': (time*parseInt(timerail_width)*1.0/duration)+'px',
 				'bottom': '-14px'});
 			
-			jQuery(this).replaceWith('<a href="#" onclick="jQuery(\'video\')[0].player.setCurrentTime('+time+');return false;">' + index + '</a>');
+			jQuery(this).replaceWith('<a href="#" onclick="jQuery(\'video\')[0].player.setCurrentTime('+time+');return false;" class="timestamp" timestamp="' + time + '">' + index + '</a>');
 		});
 	}, 500);
 });
